d55ab5f18ae5311cc7e291e5d9cf2a6bdd1847bf
kjoonha <g1004jay@snu.ac.kr>
2025-08-04 01:28:44 +0900

    move api usage source code to slang-playground.

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,5 @@
 #include <iostream>
-#include <slang/util/BumpAllocator.h>
-#include <slang/util/SmallVector.h>
-// #include <slang/util/Span.h>
 
 using namespace std;
-using namespace slang;
 
-int main() {
-    SmallVector<int> smallVec;
-    smallVec.append(2, 3);
-    smallVec.append(1, 4);
-    smallVec.append(1, 5);
-
-    for (int val : smallVec) {
-        std::cout << val << " ";
-    }
-
-    BumpAllocator alloc;
-    span<int> finalList = smallVec.copy(alloc);
-
-    for (int val : finalList) {
-        std::cout << val << " ";
-    }
-
-    return 0;
-}
+int main() { std::cout << "Hello World!" << " "; }
